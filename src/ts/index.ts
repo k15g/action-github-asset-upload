1f8d5ce5752e6a5f2c14d3693bd6cc63420ec006
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-09-01 12:03:29 +0200

    Cleaning

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -49,7 +49,7 @@ async function run() {
             core.setOutput('state', upload.data.state)
             core.setOutput('browser_download_url', upload.data.browser_download_url)
 
-            core.info("Successful upload")
+            core.info("✅ Successful upload")
         }).catch(err => {
             if (err.response.data.errors) {
                 switch (err.response.data.errors[0].code) {
